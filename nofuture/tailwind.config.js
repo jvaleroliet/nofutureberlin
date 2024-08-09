fb27e566be63e1ad6505f2fce49e2432ac7f8ad1
jvaleroliet <olietvalero@gmail.com>
2024-08-09 19:45:52 +0200

    lotofstuf

---
+++ nofuture/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+      './_drafts/**/*.html',
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_releases/**/*.html',
+      './_dates/**/*.html',
+      './_posts/*.md',
+      './*.md',
+      './*.html',
+    ],
+    theme: {
+        extend: {
+          colors: {
+            'crazybase':'#EB521A',
+          }
+        },
+    },
+    plugins: []
+  }
